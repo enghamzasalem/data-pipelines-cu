b6a62951613bab7e0379fd3e102a8542fea8d4d0
Renuka Joshi <renukajoshi3110@gmail.com>
2026-04-18 23:25:22 +0200

    Lecture 7: Webserver + n8n (final clean)

---
+++ lecture7/Assignment7-docker/main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Pull nginx image
+resource "docker_image" "nginx" {
+  name = "nginx:alpine"
+}
+
+# Create custom HTML file
+resource "local_file" "index_html" {
+  content  = "<h1>Hello Renuka</h1><p>Lecture 7 Assignment</p>"
+  filename = "${path.module}/index.html"
+}
+
+# Webserver container
+resource "docker_container" "webserver" {
+  name  = "webserver-clean"
+  image = docker_image.nginx.image_id
+
+  depends_on = [local_file.index_html]
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+
+  volumes {
+    host_path      = abspath(path.module)
+    container_path = "/usr/share/nginx/html"
+    read_only      = true
+  }
+}
+
+# Pull n8n image
+resource "docker_image" "n8n" {
+  name = "n8nio/n8n"
+}
+
+# n8n container
+resource "docker_container" "n8n" {
+  name  = "n8n-clean"
+  image = docker_image.n8n.image_id
+
+  depends_on = [docker_container.webserver]
+
+  ports {
+    internal = 5678
+    external = 5678
+  }
+}
+
+# Outputs
+output "webserver_url" {
+  value = "http://localhost:8080"
+}
+
+output "n8n_url" {
+  value = "http://localhost:5678"
+}
